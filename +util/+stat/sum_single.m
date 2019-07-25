720bdeb8698d5c9af5e05faed4bd47c23ceca5d8
guynir42 <wfast17@gmail.com>
2019-07-25 18:00:14 +0300

    Added help file to sum_single and added util.sys.match_files

---
+++ +util/+stat/sum_single.m
@@ -0,0 +1,3 @@
+% Usage: S = sum_singles(matrix)
+% Takes inputs of types double, single and uint16 and returns a sum along
+% the 3rd dimension in single precision.
---
+++ +util/+sys/match_files.m
@@ -0,0 +1,30 @@
+function files = match_files(expr, directory, full_path)
+% Usage: files = match_files(expr, directory='', full_path=0)
+% Match the glob expression "expr" in "directory" (default is empty, i.e.,
+% the current dir). Set "full_path" to true to output a list of full paths
+% to the files. 
+%
+% Returns a cell array with all matched files. 
+
+    if nargin==0, help('util.sys.match_files'); return; end
+
+    if nargin<2 || isempty(directory)
+        directory = '';
+    end
+    
+    if nargin<3 || isempty(full_path)
+        full_path = 0;
+    end
+    
+    list = dir(fullfile(directory, expr));
+    folders = {list.folder}';
+    names = {list.name}';
+    idx = ~[list.isdir]';
+    
+    if full_path
+        files = strcat(folders(idx), '/', names(idx));
+    else
+        files = names(idx);
+    end
+    
+end
