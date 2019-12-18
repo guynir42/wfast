3d9a17086d0e4b69581c5beeff2b1b6d65f1ed21
guynir42 <wfast17@gmail.com>
2019-12-18 10:12:37 +0200

    added +occult/stellar_distance

--- +util/+img/compile_mex_cutout.m
+++ +util/+img/compile_mex_cutout.m
@@ -1,5 +1,5 @@
 import util.text.sa;
 
-str = ['mex ' sa(getenv('MAT'), '+util/+img/mexCutout.cpp') ' -outdir ' sa(getenv('MAT'), '+util/+img')];
+str = ['mex ' sa(getenv('WFAST'), '+util/+img/mexCutout.cpp') ' -outdir ' sa(getenv('WFAST'), '+util/+img')];
 
 eval(str);
