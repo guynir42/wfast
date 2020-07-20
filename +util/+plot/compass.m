f30f5a0de3fa9671238021005168956733ab7b13
guyn <guyynir@gmail.com>
2020-07-20 15:13:55 +0300

    added a new plotting function: compass() to show N and E directions

---
+++ +util/+plot/compass.m
@@ -0,0 +1,107 @@
+function ax = compass(side, varargin)
+% Usage: ax = compass(side, varargin)
+% Draw a small, transparent axes with arrows pointing North and East, based
+% on the camera angle of the Balor. 
+% The first input, which is mandatory, is the side of the telescope, either
+% East or West, which tells if the compass needs to be rotated by 180 deg. 
+%
+% OPTIONAL ARGUMENTS
+%   -angle: the angle of cardinal North relative to camera top, going 
+%           clockwise. The default (-60) is for the Balor, and means the 
+%           North direction is left of the camera top (negative x, positive
+%           y direction) by 60 degrees. Defined for telescope pointing East.
+%   -figure/parent: handle to the panel or figure where the axes should be
+%                   added to. Will try to erase previously drawn compasses. 
+%   -corner: Choose SouthEast (default), NorthEast, NorthWest or SouthWest. 
+%   -margin: the distance from the corner of the parent (Normalized units). 
+%            The default value is 0.1. If scalar, set same to x and y. 
+%   -size: In normalized units. The compass is always square. Default is 0.1. 
+%   -color: of the arrows. Default is 'red'. 
+%   -font_size: for the N and W letters. Default is 0.25 (normalized units). 
+%
+
+    import util.text.cs; 
+    
+    if nargin==0, help('util.plot.compass'); return; end
+    
+    input = util.text.InputVars;
+    input.input_var('angle', -60); % between North and camera top, going clockwise (for telescope East)
+    input.input_var('figure', [], 'parent'); 
+    input.input_var('corner', 'SouthEast', 'position'); 
+    input.input_var('margin', 0.1); 
+    input.input_var('size', 0.1); 
+    input.input_var('color', 'red'); 
+    input.input_var('font_size', 0.25); 
+    input.scan_vars(varargin{:}); 
+    
+    if cs(side, 'East')
+        % do nothing?
+    elseif cs(side, 'West')
+        input.angle = input.angle + 180;
+    else
+        error('Unknown side: "%s". Use "East" or "West" for the telescope pointing', side); 
+    end
+    
+    if isempty(input.figure)
+        input.figure = gcf;
+    end
+    
+    delete(findobj(input.figure, 'Tag', 'util.plot.compass'))
+    
+    if isscalar(input.margin)
+        input.margin = input.margin.*[1 1];
+    end
+        
+    if cs(input.corner, 'SouthEast')
+        pos(1) = 1 - input.margin(2) - input.size; % left side
+        pos(2) = input.margin(1); % top side
+    elseif cs(input.corner, 'SouthWest')
+        pos(1) = input.margin(2); % left side
+        pos(2) = input.margin(1); % top side
+    elseif cs(input.corner, 'NorthEast')
+        pos(1) = 1 - input.margin(2) - input.size; % left side
+        pos(2) = 1 - input.margin(1) - input.size; % top side
+    elseif cs(input.corner, 'NorthWest')        
+        pos(1) = input.margin(2); % left side
+        pos(2) = 1 - input.margin(1) - input.size; % top side
+    else
+        error('Unknown "corner" input "%s". Use "NorthEast", "NorthWest", "SouthEast" or "SouthWest"', input.corner); 
+    end
+    
+    pos(3) = input.size; % width
+    pos(4) = input.size; % height
+    
+    ax = axes('Parent', input.figure, 'Position', pos); 
+    
+    quiver(ax, [0 0], [0 0],...
+        [sind(input.angle) sind(input.angle+90)], ...
+        [cosd(input.angle) cosd(input.angle+90)], ...
+        '-', 'filled', 'LineWidth', 1.5, 'MaxHeadSize', 1, ...
+        'Color', input.color)
+    
+    Nx = sind(input.angle); 
+    Ny = cosd(input.angle); 
+    Ex = sind(input.angle+90); 
+    Ey = cosd(input.angle+90); 
+    
+    text(ax, Nx, Ny, 'N', 'FontUnits', 'Normalized', 'FontSize', input.font_size, 'Color', input.color,...
+        'Rotation', -input.angle, 'HorizontalAlignment', 'center', 'VerticalAlignment', 'bottom'); 
+    text(ax, Ex, Ey, 'E', 'FontUnits', 'Normalized', 'FontSize', input.font_size, 'Color', input.color,... 
+        'Rotation', -input.angle, 'HorizontalAlignment', 'left', 'VerticalAlignment', 'middle');  
+    
+    ax.Tag = 'util.plot.compass'; 
+    ax.XLim = [-1 1];
+    ax.YLim = [-1 1]; 
+    axis(ax, 'square'); 
+    ax.Color = 'none';
+    ax.XColor = 'none';
+    ax.YColor = 'none'; 
+    
+    if nargout==0
+        clear ax;
+    end
+    
+end
+
+
+
