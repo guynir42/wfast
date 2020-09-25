9c17bbb39b319d95251e177edca1769b034ce132
guyn <guyynir@gmail.com>
2020-09-25 16:58:42 +0300

    added geosat_summary script, but it is still empty

---
+++ scripts/geosat_summary.m
@@ -0,0 +1,11 @@
+% this script reads a list of pre-vetted micro_flare files
+% it should compile them into a list of satellite appearances that can be 
+% summarized in the paper. 
+
+% this defines the list of observations: 
+
+% runs = {'2020-09-18\SGR1806_run1', '2020-09-17\SGR1806_run1', ...
+%         '2020-09-17\ecliptic_run1', 
+
+% we can use all data from 2020-08-25 onwards
+% 
